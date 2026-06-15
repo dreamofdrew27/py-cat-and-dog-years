ee83cdc5fbfe1f9ea5be77d2b3edb68038428feb
Andrii Pavliuk <andriy270897@gmail.com>
2026-06-16 01:21:55 +0300

    Added all the requested changes

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
-    def calc(age: int, step: int) -> int:
-        if age < 15:
-            return 0
-        if age < 24:
-            return 1
-        return 2 + (age - 24) // step
-    return [calc(cat_age, 4), calc(dog_age, 5)]
+    if cat_age < 0 or dog_age < 0:
+        raise ValueError
+    else:
+        def calc(age: int, step: int) -> int:
+            if age < 15:
+                return 0
+            if age < 24:
+                return 1
+            return 2 + (age - 24) // step
+        return [calc(cat_age, 4), calc(dog_age, 5)]
--- app/test_main.py
+++ app/test_main.py
@@ -4,12 +4,15 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
-    "cat_age,dog_age,result",
+    "cat_age,dog_age,expected",
     [
         pytest.param(0, 0, [0, 0],
                      id="should return zero before"
                         " any age transitions"),
         pytest.param(14, 14, [0, 0],
+                     id="should calculate human age correctly "
+                        "before first transition"),
+        pytest.param(15, 14, [1, 0],
                      id="should calculate human age correctly "
                         "before first transition"),
         pytest.param(15, 15, [1, 1],
@@ -29,12 +32,24 @@ from app.main import get_human_age
                         "approach after the third transition"),
         pytest.param(100, 100, [21, 17],
                      id="should calculate human age "
-                        "correctly for large values")
+                        "correctly for large values"),
+        pytest.param(1000, 1000, [246, 197],
+                     id="should calculate human age "
+                        "correctly for extra large values"),
+        pytest.param(-14, 15, ValueError,
+                     id="should return error when at least one of ages "
+                        "is less than 0"),
+        pytest.param("15", 15, TypeError,
+                     id="should return error when input is not int type")
     ]
 )
 def test_should_convert_cat_and_dog_age_to_human_age(
         cat_age: int,
         dog_age: int,
-        result: list
+        expected: list[int] | Exception
 ) -> None:
-    assert get_human_age(cat_age, dog_age) == result
+    if isinstance(expected, type) and issubclass(expected, Exception):
+        with pytest.raises(expected):
+            get_human_age(cat_age, dog_age)
+    else:
+        assert get_human_age(cat_age, dog_age) == expected
